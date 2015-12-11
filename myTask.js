a8cc4ff0dfe3c2f547f294eee7b51b7a6c1befde
katerynaz15 <zbsht15@gmail.com>
2015-12-11 14:30:59 -0800

    Create a job queue whose workers fetch data from a URL and store the results in a database.  The job queue should expose a REST API for adding jobs and checking their status / results.
     Example:
    
    User submits www.google.com to your endpoint.  The user gets back a job
    id. Your system fetches www.google.com (the result of which would be
    HTML) and stores the result.  The user asks for the status of the job
    id and if the job is complete, he gets a response that includes the
    HTML for www.google.com

---
+++ myTask.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+    
+    var http = new XMLHttpRequest();
+    var url = "jobs.json";
+    http.open("GET", url, true);
+    http.send();
+    
+    var myTable = document.createElement("table");
+    document.body.appendChild(myTable);
+    
+    http.onload = function() {
+        var myObject = JSON.parse(this.responseText);
+        
+        var myEvent = document.getElementById("myForm");
+        myEvent.onsubmit = function() {
+            console.log(this);
+            var inputValue = this[0].value;
+            this[0].value = "";
+            console.log(inputValue);
+            
+            var myArray = myObject.filter(function (el) {return el.url==inputValue}); //pick the data only reletated to the web site we entered
+            console.log(myArray);
+            
+            myTable.innerHTML = "";
+            for(var i=0; i<myArray.length; i++) {
+                var myTr = document.createElement("tr");
+                myTable.appendChild(myTr);
+                for(var j in myArray[i]) {
+                    if(j=="job_ID" || j=="name") {
+                        var myTd = document.createElement("td");
+                        myTr.appendChild(myTd);
+                        myTd.innerHTML = myArray[i][j];
+                    }
+                }
+                myTr.onclick = function() {
+                    console.log(this);
+                    alert(myArray[this.rowIndex].html);
+                }
+            }
+            return false; //not to load the page when the form is submitted
+        }
+    }
+}
